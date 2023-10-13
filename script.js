81ba27832f7b807c2c065b9ec0f6986a528d645c
okediarthur <okediarthur@gmail.com>
2023-10-13 11:40:52 +0300

    Javascript Files added

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    const choices = ["rock","paper","scissors"];
+    const randomIndex = Math.floor(Math.round() * choices.length);
+    return choices[randomIndex];
+}
+
+function playerSelection(){
+    const playerChoice = prompt("Input choice(Rock, Paper, Scissors):");
+}
+
+function playRound(){
+    
+}
