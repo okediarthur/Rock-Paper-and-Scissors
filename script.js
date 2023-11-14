3d0d82cf151c065996bd276c646455a2b1bd69ca
okediarthur <okediarthur@gmail.com>
2023-11-14 11:50:29 +0300

    Js Code Additions

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function getcomputerChoice() {
 }
 
 function playRound(playerSelection, computerChoice){
+    
     if (playerSelection === computerChoice) {
         return("It's a tie!");
     } else if (playerSelection === 'rock' && computerChoice === 'scissors'){
@@ -16,38 +17,66 @@ function playRound(playerSelection, computerChoice){
     } else {
         return "Computer wins!";
     }
+    
 }
-
-// function playGame(){
-//     const playerSelection = document.getElementById("userChoice").value.toLowerCase();
-//     const computerChoice = getcomputerChoice();
-//     const result = playRound(playerSelection, computerChoice);
-//     const resultElement = document.getElementById("result");
-
-//     resultElement.textContent = result;
-// }
-
-let gameResults = [];
-
-let gameCount = 0;
 
 function game(){
-    if(gameCount < 5){
-    const playerSelection = document.getElementById("userChoice").value.toLowerCase();
-    const computerChoice = getcomputerChoice();
-    const result = playRound(playerSelection, computerChoice);
-    
-    gameResults.push(result);
 
-    gameCount++;
+    // let gameResults = [];
+    let computerScore = 0;
+    let playerScore = 0;
+    let gameCount = 0;
 
-    const resultElement = document.getElementById("result");
-    resultElement.textContent = result;
-
-    if(gameCount === 5){
-        resultElement.textContent += "\nGame Over! Results: " + gameResults.join(", ");
-    } else {
-        resultElement.textContent = "Game Over! Results: " + gameResults.join(", ");
+    function updateScore(result){
+        if(result.includes("You win")){
+            playerScore++;
+        }else if(result.includes("Computer wins")){
+            computerScore++;
+        }
     }
+
+    function checkGameCompletion(){
+        if(gameCount <= 5){
+            console.log("Game Over. Results:", "Player: " + playerScore, "Computer: " + computerScore); 
+            if(playerScore > computerScore){
+                console.log("Overall Winner: Player!")
+            } else if(computerScore > playerScore){
+                console.log("Overall Winner: Computer!")
+            }else {
+                console.log("It's a tie in the overall game!");
+            }
+        }
+    }
+
+    let rock = document.querySelector('#rock');
+    rock.addEventListener('click', function() {
+        var computerChoice = getcomputerChoice();
+        var result = playRound("rock", computerChoice);
+        document.getElementById("result").innerText = result;
+        updateScore(result);
+        gameCount++;
+        checkGameCompletion();
+    });
+
+    let paper = document.querySelector('#paper');
+    paper.addEventListener('click', function() {
+        var computerChoice = getcomputerChoice();
+        var result = playRound("paper", computerChoice);
+        document.getElementById("result").innerText = result;
+        updateScore(result);
+        gameCount++;
+        checkGameCompletion();
+    });
+
+    let scissors = document.querySelector('#scissors');
+    scissors.addEventListener('click', function() {
+        var computerChoice = getcomputerChoice();
+        var result = playRound("scissors", computerChoice);
+        document.getElementById("result").innerText = result;
+        updateScore(result);
+        gameCount++;
+        checkGameCompletion();
+    });
 }
-}
+
+game();
